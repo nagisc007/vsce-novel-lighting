6c2e72552f8ede0f8bf50b419bbbef705ea1a968
N.T.WORKS <nagisc007@yahoo.co.jp>
2022-12-26 16:53:02 +0900

    Add char count in extension file.

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,18 @@
 "use strict";
 import * as vscode from 'vscode';
+import { CharCounter, CharCountUpdater } from './charcounter';
 
 export function activate(context: vscode.ExtensionContext) {
 
+	// クラス作成
+	const counter = new CharCounter();
+	const countUpdater = new CharCountUpdater(counter);
+
+	// 登録
+	context.subscriptions.push(countUpdater);
+    context.subscriptions.push(counter);
+	
+	// テスト用
 	let disposable = vscode.commands.registerCommand('NVL.helloworld', () => {
 		// 第二、第三引数でメッセージの右側にボタン表示
 		vscode.window.showInformationMessage('Hello World from Novel-Lighting!', '了解', 'キャンセル')
